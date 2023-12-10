8cf6da0524c56361ae674056e7aac0df69216d14
Brian Roepke <broepke@gmail.com>
2023-12-10 05:44:29 -0600

    Clean Up

--- deployments.sh
+++ deployments.sh
@@ -4,3 +4,10 @@ prefect deployment build ec2/ec2_instance_refresh.py:refresh_instances \
     --work-queue default  \
     --apply \
     --skip-upload
+
+prefect deployment build deadpool/deadpool.py:dead_pool_status_check \
+    --name deadpool-github \
+    --storage-block github/github-repo \
+    --work-queue default  \
+    --apply \
+    --skip-upload
