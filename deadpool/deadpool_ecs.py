a3966b6b3e98a033e5e18e748c071b2061cefd62
Brian Roepke <broepke@gmail.com>
2024-10-28 13:31:07 -0400

    Added flags for future use

--- deadpool/deadpool_ecs.py
+++ deadpool/deadpool_ecs.py
@@ -201,6 +201,8 @@ if __name__ == "__main__":
         name="deadpool-ecs-deployment",
         work_pool_name="dka-ecs-pool",
         cron="0 17 * * *",
+        build=True,
+        push=True,
         image=DockerImage(
             name="222975130657.dkr.ecr.us-east-1.amazonaws.com/prefect-flows:latest",
             platform="linux/amd64",
