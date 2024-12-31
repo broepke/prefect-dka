c5488ae0d871ef1d6980994e9d5c07c668f93b64
Brian Roepke <broepke@gmail.com>
2024-12-31 09:33:24 -0600

    Fixed the Prefect Managed Pool deployment code - this is running now

--- deadpool/deadpool.py
+++ deadpool/deadpool.py
@@ -208,20 +208,36 @@ def dead_pool_status_check():
 #     )
 
 # Local Testing
-if __name__ == "__main__":
-    dead_pool_status_check()
-    
-# Prefect Managed Work Pool
 # if __name__ == "__main__":
-#     dead_pool_status_check.from_source(
-#         source="https://github.com/broepke/prefect-dka.git",
-#         entrypoint="deadpool/deadpool.py:dead_pool_status_check",
-#     ).deploy(
-#         name="deadpool-managed-deployment",
-#         work_pool_name="dka-managed-pool",
-#         work_queue_name="dka-managed-queue",
-#         job_variables={"pip_packages": ["prefect-snowflake", "prefect-slack", "prefect-github", "prefect-aws", "twilio", "sendgrid", "snowflake-connector-python[pandas]"]},
-#         cron="0 17 * * *",
-#     )
-    
+#     dead_pool_status_check()
 
+# Prefect Managed Work Pool
+if __name__ == "__main__":
+    dead_pool_status_check.from_source(
+        source="https://github.com/broepke/prefect-dka.git",
+        entrypoint="deadpool/deadpool.py:dead_pool_status_check",
+    ).deploy(
+        name="deadpool-managed-deployment",
+        work_pool_name="dka-managed-pool",
+        work_queue_name="dka-managed-queue",
+        job_variables={
+            "pip_packages": [
+                "prefect",
+                "prefect-snowflake",
+                "prefect-shell",
+                "prefect-slack",
+                "prefect-github",
+                "prefect-aws",
+                "prefect-docker",
+                "prefect[docker]",
+                "asyncpg",
+                "s3fs",
+                "twilio",
+                "sendgrid",
+                "SPARQLWrapper",
+                "snowflake-connector-python[pandas]",
+            ],
+            "env": {"PREFECT_LOGGING_LEVEL": "ERROR"},
+        },
+        cron="0 17 * * *",
+    )
